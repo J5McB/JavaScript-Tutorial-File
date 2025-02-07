d0fe9b1ac955122ab66f4a6ffeb66a53afedea62
J5McB <forever5trg@gmail.com>
2025-02-07 04:40:42 -0500

    Add HTML template for JavaScript beginner's tutorial

---
+++ index.js
@@ -0,0 +1,136 @@
+        //  1. Different types of printing in JavaScript
+        // console.log("Hello World");   
+        // alert("modification");
+        // document.write("Document write is here")
+        // console.warn("This is a warning");
+        // console.error("This is an error");
+        
+        // //  2. The JavaScript Console API (Application Programming Interface) provides methods for interacting with the web browser's console. The console is part of the browser's developer tools, and is primarily used for debugging purposes.
+        // console.log("Hello");   
+        // // alert("modification");
+        // // document.write("Document write is here")
+        // console.warn("This is a warning");
+        // console.error("This is an error");
+   
+        //  3. JavaScript Variables        
+        //  JavaScript is like a container to store differrent types of data.
+        //  1. Text or String 2. Number 3. Boolean 4. Array 5. Object 6. Null 7. Undefined 
+        // var Number1 = 95;
+        // var Number2 = 75;
+        // console.log(Number1 - Number2);
+        
+        /*
+         4. Comments in JavaScript
+         Put outlogical barriers in the second line of the h1 tag
+         4. Comments in JavaScript
+          Put outlogical barriers in the second line of the h1 tag
+        */
+        
+         // 5. Different Data Types
+      
+         // String
+         // var Text1 = 'This is String One';
+         // var Text2 = 'This is String Two';
+
+         // Number
+         // var Number1 = 66
+         // var Number2 = 88
+         // console.log(Number1)
+
+      // Object
+      var CBC = {
+              Hb :23,
+              WBC:76,
+              RBC:75,
+              Platelets: 64,
+              ESR:11
+
+        
+    
+      }
+      
+      // Boolean Values
+      var x = true
+      var y = false
+
+      // Undefined variable
+      var abc;
+      console.log(abc)
+
+      //Null Data Type
+      var na = null
+      console.log(na)
+     /*
+     At the higher level data types are divided into two categories
+     1. Primitive Data Types- Number, String, Boolean, Undefined, Null, Symbols
+     2. Reference Data Types- Arrays and Objects
+     */
+     var Array = [6, 8, 22.76, 'Nathan', true, "Brant"]
+      console.log(Array)
+
+    // Index numbering
+    // a b c d e f
+    // 0 1 2 3 4 5
+
+    // 6. Operators
+
+    // Arithmetic Operators
+    // a = 60
+    // b = 3
+    // console.log("The value of a + b is", a + b)
+    // console.log("The value of a + b is", a - b)
+    // console.log("The value of a + b is", a * b)
+    // console.log("The value of a + b is", a / b)
+
+// Comparative Operators
+// var ab = 72
+// var ba = 20
+// console.log(ab == ba)
+// console.log(ab >= ba)
+// console.log(ab <= ba)
+// console.log(ab < ba)
+// console.log(ab > ba)
+
+// Assignment Operators
+var c = ab
+// c += 3 
+// c -= 3
+// c *= 3
+// c /= 3 
+// console.log(c)
+
+// Logical Operators
+
+// 1. and
+// console.log(true && true) // true
+// console.log(true && false) // false
+// console.log(false && true) // false
+// console.log(false && false) // false
+
+// 2. or
+// console.log(true || true) // true
+// console.log(true || false) // true
+// console.log(false || true) // true
+// console.log(false || false) // false
+
+// 3. not
+// console.log(!true) // false
+// console.log(!false) // true
+
+// 7. Functions in JavaScript
+function avg(a, b, c) {
+        return (a + b + c) / 3;
+}
+
+a1 = avg (22 + 34 + 44)/3;
+b1 = avg (10 + 14 + 18)/3
+c1 = avg (40 + 50 + 60)/3
+console.log(a1, b1, c1)
+
+
+
+
+
+
+    
+
